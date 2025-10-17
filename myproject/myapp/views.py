814a511c147030ef22d535d8f0e525c929af3fc5
anurag12anu <149938549+anurag12anu@users.noreply.github.com>
2025-10-17 18:57:27 +0530

    modified file

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -59,6 +59,57 @@ class CustomerProfileDetailView(APIView):
             return Response({'errors':'customer profile not find'}, status=status.HTTP_404_NOT_FOUND)
         profile.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RoomListCreateView(APIView):
+    
+    def get(self, request):
+        room_data=Room.objects.all()
+        serializer=RoomSerializer(room_data,many=True)
+        return Response(serializer.data)
+    
+
+    
+    def post(self,request):
+        serializer=RoomSerializer(data=request.data)
         
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class RoomDetailView(APIView):
+    
+    def get_object(self,pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            return None
+    
+    def get(self,request,pk):
+        room=self.get_object(pk)
+        if room is None:
+            return Response({'errors','Room not found'},status=404)
         
+        serializer=RoomSerializer(room)
+        
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        room=self.get_object(pk)
+        
+        serializer=RoomSerializer(room,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=400)
+    def delete(self,request,pk):
+        room=self.get_object(pk)
+        
+        if room is None:
+            return Response({'errors','Room not found'})
+        room.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+            
+
             
